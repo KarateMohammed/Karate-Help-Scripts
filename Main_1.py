bb1a3f65787cff66ac756d66bd881978a7c9623a
KarateMohammed <karate.mohammed@yahoo.com>
2021-06-22 18:09:43 +0000

    test Dict

--- Main_1.py
+++ Main_1.py
@@ -24,6 +24,7 @@ print("dict_value")
 print(dict_value)
 print("")
 
+
 print("num.items")
 print(num.items())
 print("")
@@ -33,6 +34,11 @@ print(num.keys())
 Dict_Keys=num.keys()
 for i in  Dict_Keys :
 	print (i)
+
+print("num.items")
+for x,y in num.items():
+	print(f"Key {x} Value {y}")
+
 
 Temp_Dict_Username={"odd":11}
 Temp_Dict_Pass ={"even":''}
@@ -52,23 +58,94 @@ print("")
 print("num")
 print(num)
 
-# num=[["a",1,2],["b",3,4],["c",5,6],["d",7,8],["s",9,10]]
-# n="n"
-# x=[]
-# y=[]
-# for i in num :
-# 	print(i)
-# 	x.append(i[0])
-		
-# print(x)
-# for q in num :
-# 	for w in q :
-# 		if n not in w :
-# 			print("False")
-# 			y.append(n,m,o)
-# print ("y")
-# print (y)
+print('')
+print(num['a']['odd'])
 
-# current_date = datetime.date.today().strftime("%y-%m-%d")
-# print (current_date)
-# print (type(current_date))
+num.update({"l":{"odd":1003,"even": 1000}})
+
+import os
+import os.path
+import json
+
+def Append_to_Old_File(fullpath='' ,Unknown_Lists=[]) :
+	with open(fullpath, 'a') as file:
+		for i in Unknown_Lists:
+			file.write((str(i)+"\n"))
+	os.chmod(fullpath, 0o777)  ## to use it with full permisson
+	file.close()
+
+
+Dict_all_IP_Usr_Pass_Ena={}
+ip="123"
+Temp_Dict_k_ip={}
+Temp_Dict_k_ip[ip]={}
+Device_Type=[]
+Username_Device=[]
+Passowrd_Device=[]
+Device_Type_Num=0
+User_Pass_Num=0
+Device_Type=['mohammed']
+Passowrd_Device=['Mohammed']
+Username_Device=['Password']
+Temp_Dict_Connection_type={"Connection_type":Device_Type[Device_Type_Num]}
+Temp_Dict_k_Username={"k_Username":Username_Device[User_Pass_Num]}
+Temp_Dict_k_Password={"k_Password":Passowrd_Device[User_Pass_Num]}
+Temp_Dict_k_Enable={"k_Enable":''}
+
+Temp_Dict_k_ip[ip].update(Temp_Dict_Connection_type)
+Temp_Dict_k_ip[ip].update(Temp_Dict_k_Username)
+Temp_Dict_k_ip[ip].update( Temp_Dict_k_Password)
+Temp_Dict_k_ip[ip].update(Temp_Dict_k_Enable)
+Dict_all_IP_Usr_Pass_Ena.update(Temp_Dict_k_ip)
+print (f"Dict_all_IP_Usr_Pass_Ena[{ip}]")
+print (Dict_all_IP_Usr_Pass_Ena[ip])
+# Temp_Dict_k_ip.popitem()
+Temp_Dict_k_ip={}
+print("After POP Temp_Dict_k_ip")
+print(Temp_Dict_k_ip)
+
+fullpath='/home/khayat/Test_New_Auto/Dict_IPs.json'
+
+Dict_all_IP_Usr_Pass_Ena={"a":{"odd":1,"even": 2},"b" :{"odd":3 ,"even": 4},"c":{"odd":5,"even": 6},"d":{"odd":7,"even": 8},"s":{"odd":9,"even": 10}}
+
+data=json.dumps(Dict_all_IP_Usr_Pass_Ena)
+with open(fullpath, 'a') as convert_file:
+	convert_file.write(data)
+os.chmod(fullpath, 0o777)  ## to use it with full permisson
+convert_file.close()
+
+Temp_Dict_fake={"h":{'odd':33 ,'even':66}}
+# Temp_Dict_fake= {'a':{'odd':333 ,'even':666}}
+Temp_Dict_fake_Key= [*Temp_Dict_fake.keys()]
+
+
+
+
+with open(fullpath) as convert_file:
+	data=convert_file.read()
+convert_file.close()
+os.chmod(fullpath, 0o777)  ## to use it with full permisson
+data=json.loads(data)
+print("Dict:", data)
+print("Type:", type(data))
+
+if not Temp_Dict_fake_Key[0] in Dict_all_IP_Usr_Pass_Ena.keys():
+	print("A new Key ")
+	print("Temp_Dict_fake.keys()")
+	print(Temp_Dict_fake.keys())
+
+	data=json.dumps(Temp_Dict_fake)
+	with open(fullpath, 'a') as convert_file:
+		convert_file.write(data)
+	os.chmod(fullpath, 0o777)  ## to use it with full permisson
+	convert_file.close()
+
+else :
+	print("An old Key ")
+
+
+
+# # Print the data of dictionary
+# print("\nPeople1:", data['a'])
+# print("\nPeople2:", data['b'])
+# # Append_to_Old_File(fullpath,)
